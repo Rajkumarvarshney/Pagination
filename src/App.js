83656da869d77ce5a5a2f3033b791fa350ce4d50
Rajkumar Varshney <raj1511aryan@gmail.com>
2023-01-04 15:18:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://dummyjson.com/products?limit=100`)
+      const data = await res.json()
+      console.log(data)
+      if (data && data.products) {
+        setData(data.products)
+      }
+    }
+    fetchData()
+
+  }, [])
+  const selectedPageHandler = (selectedPage) => {
+    setPage(selectedPage)
+
+  }
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pagination </h1>
+      {data.length > 0 && <div className="products">
+        {
+          data.slice(page * 10 - 10, page * 10).map((ele) => {
+            return (
+              <>             <img src={ele.thumbnail} />
+
+              </>
+
+            )
+          })
+        }
+      </div>}
+      {
+        data.length > 0 && <div className="pagination">
+          <span onClick={() => selectedPageHandler(page - 1)} className={page - 1 >= 1 ? " " : "pagination_disable"}>L</span>
+          {[...Array(data.length / 10)].map((_, i) => {
+            return (
+              <span className={page == i + 1 ? "pagination_selected" : " "} onClick={() => { selectedPageHandler(i + 1) }}>{i + 1}</span>
+            )
+
+          })}
+
+          <span onClick={() => selectedPageHandler(page + 1)} className={page + 1 <= data.length / 10 ? " " : "pagination_disable"}>R</span>
+        </div>
+      }
     </div>
   );
 }
